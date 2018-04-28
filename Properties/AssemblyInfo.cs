41030767aeb6bf5b02d7bdfd35a4598ab4dd8f33
rik cousens <rcousens@colt.com>
2018-04-27 20:28:05 -0400

    initial deposit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly:AssemblyTitle("LaserCfg")]
+[assembly:AssemblyProduct("LaserCfg")]
+[assembly:AssemblyDescription("description of LaserCfg.")]
+[assembly:AssemblyCompany("Colt Manufacturing Company LLC")]
+[assembly:AssemblyCopyright("Copyright © 2018, Colt Manufacturing Company LLC")]
+#if DEBUG
+[assembly:AssemblyConfiguration("Debug version")]
+#else
+[assembly:AssemblyConfiguration("Release version")]
+#endif
+[assembly:ComVisible(false)]
+
+[assembly:AssemblyVersion("1.0.0.0")]
+
